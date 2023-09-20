0b23e900e759f07b6ae40ea1e58b722b342df173
FAIQ-HASSSAN <130917020+FAIQ-HASSSAN@users.noreply.github.com>
2023-09-20 23:59:20 +0500

    Update Faculty.java

--- src/questiontwo/Faculty.java
+++ src/questiontwo/Faculty.java
@@ -1,20 +1,30 @@
 package questiontwo;
 
 public class Faculty extends Staff {
-	
-	public String facultyspecializtion;
-	public String Section;
-	public int NoOfStudents;
-	public String Qualification;
-	
-	public String Tech(String course)
-	{
-		return facultyspecializtion;
-	}
-	
-	public String Assess(String studentgrade)
-	{
-		return null;
-	}
+    public String facultyspecialization;
+    public String Section;
+    public int NoOfStudents;
+    public String Qualification;
 
+    public String Tech(String course) {
+        // Implement teaching specific to a course
+        return "Teaching course: " + course;
+    }
+
+    public String Assess(String studentgrade) {
+        // Implement assessment logic
+        return "Assessing student grade: " + studentgrade;
+    }
+}
+
+// Pgcstudent.java
+package questiontwo;
+
+public class Pgstudent extends Student {
+    public String rollno;
+    public String specialization;
+
+    public void SubmitThesis() {
+        // Implement thesis submission logic
+    }
 }
