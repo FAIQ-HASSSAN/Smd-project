ed5e4d1fe19b80156604ba33c3d1e947f839c380
FAIQ-HASSSAN <130917020+FAIQ-HASSSAN@users.noreply.github.com>
2023-09-20 23:58:23 +0500

    Update Student.java

--- src/questiontwo/Student.java
+++ src/questiontwo/Student.java
@@ -1,55 +1,54 @@
 package questiontwo;
 
 public class Student extends Person {
-	
-	public String studentid;
-	public String Major;
-	public int Year;
-	public int Semester;
-	public String college;
-	public int grade;
-	
-	public String Register(String course)
-	{
-		return null;
-	}
+    public String studentid;
+    public String Major;
+    public int Year;
+    public int Semester;
+    public String college;
+    public int grade;
 
-	public int Getgrade(String course)
-	{
-		return grade;
-	}
-	
-	public String Attend(String Class)
-	{
-		return null;
-	}
-	
-	public String Learn()
-	{
-		return null;
-	}
+    public String Register(String course) {
+        // Implement registration logic here
+        return "Registered for course: " + course;
+    }
 
-	@Override
-	public String Register(int uid) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public int Getgrade(String course) {
+        // Implement getting the grade for a specific course here
+        return grade;
+    }
 
-	@Override
-	public String Perform(String duty) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public String Attend(String Class) {
+        // Implement attendance logic here
+        return "Attending class: " + Class;
+    }
 
-	@Override
-	public String include(String service) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public String Learn() {
+        // Implement learning logic here
+        return "Learning...";
+    }
 
-	@Override
-	public String like(String item) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public String Register(int uid) {
+        // TODO: Implement registration based on UID
+        return null;
+    }
+
+    @Override
+    public String Perform(String duty) {
+        // TODO: Implement student-specific duties
+        return null;
+    }
+
+    @Override
+    public String include(String service) {
+        // TODO: Implement including a service
+        return null;
+    }
+
+    @Override
+    public String like(String item) {
+        // TODO: Implement liking an item
+        return null;
+    }
 }
