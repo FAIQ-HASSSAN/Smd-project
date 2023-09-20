6c3957a03a2a96b11b60f3d12928b0660d1a880b
FAIQ-HASSSAN <130917020+FAIQ-HASSSAN@users.noreply.github.com>
2023-09-20 23:58:55 +0500

    Update Staff.java

--- src/questiontwo/Staff.java
+++ src/questiontwo/Staff.java
@@ -1,55 +1,47 @@
 package questiontwo;
 
 public class Staff extends Person {
-	
-	public String employeeid;
-	public String employeerole;
-	public String department;
-	public float salary;
-	public int experience;
-	
-	public String Attend(String duty)
-	{
-	  return null;	
-	}
+    public String employeeid;
+    public String employeerole;
+    public String department;
+    public float salary;
+    public int experience;
 
-	
-	public String GetPromotion()
-	{
-	  return null;	
-	}
-	
-	public int GetSalary()
-	{
-	  return (int) salary;	
-	}
+    public String Attend(String duty) {
+        // Implement attendance logic here
+        return "Attending duty: " + duty;
+    }
 
+    public String GetPromotion() {
+        // Implement promotion logic here
+        return "Promoted!";
+    }
 
-	@Override
-	public String Register(int uid) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public int GetSalary() {
+        return (int) salary;
+    }
 
+    @Override
+    public String Register(int uid) {
+        // TODO: Implement registration based on UID
+        return null;
+    }
 
-	@Override
-	public String Perform(String duty) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public String Perform(String duty) {
+        // TODO: Implement staff-specific duties
+        return null;
+    }
 
+    @Override
+    public String include(String service) {
+        // TODO: Implement including a service
+        return null;
+    }
 
-	@Override
-	public String include(String service) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
-	@Override
-	public String like(String item) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public String like(String item) {
+        // TODO: Implement liking an item
+        return null;
+    }
 }
-
