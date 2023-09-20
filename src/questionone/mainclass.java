964d78d7f1cf138a6783ee0bf033032c2a46c10c
FAIQ-HASSSAN <130917020+FAIQ-HASSSAN@users.noreply.github.com>
2023-09-20 23:53:06 +0500

    mainclass.java

--- src/questionone/mainclass.java
+++ src/questionone/mainclass.java
@@ -1,11 +1,15 @@
 package questionone;
 
+public class MainClass {
+    public static void main(String[] args) {
+        Allrounder allrounder = new Allrounder("John", 25, "Country");
 
-public class mainclass {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+        // Testing the methods and properties
+        allrounder.print();
+        System.out.println("Name: " + allrounder.name);
+        System.out.println("Age: " + allrounder.age);
+        System.out.println("Nationality: " + allrounder.nationality);
+        System.out.println("Batsman Ranking: " + allrounder.getRanking());
+        System.out.println("Bowler Ranking: " + allrounder.getRanking());
+    }
 }
