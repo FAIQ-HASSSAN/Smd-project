817b1fcb6df58d1e6573dd3b7bb445c64a72a272
FAIQ-HASSSAN <130917020+FAIQ-HASSSAN@users.noreply.github.com>
2023-09-21 00:00:04 +0500

    Update Mainclass.java

--- src/questiontwo/Mainclass.java
+++ src/questiontwo/Mainclass.java
@@ -1,10 +1,7 @@
 package questiontwo;
 
 public class Mainclass {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        // You can create instances of your classes and test their methods here
+    }
 }
