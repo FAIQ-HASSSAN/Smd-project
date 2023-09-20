2d09213233b6c15ed74fdcd812e1f3f92bc35f71
FAIQ-HASSSAN <130917020+FAIQ-HASSSAN@users.noreply.github.com>
2023-09-20 23:59:45 +0500

    Update Technician.java

--- src/questiontwo/Technician.java
+++ src/questiontwo/Technician.java
@@ -1,16 +1,15 @@
 package questiontwo;
 
 public class Technician extends Staff {
-	
-	public String techtype;
-	
-	public String Maintain(String lab)
-	{
-		return null;
-	}
+    public String techtype;
 
-	public String Install(String system)
-	{
-		return null;
-	}
+    public String Maintain(String lab) {
+        // Implement lab maintenance logic
+        return "Maintaining lab: " + lab;
+    }
+
+    public String Install(String system) {
+        // Implement system installation logic
+        return "Installing system: " + system;
+    }
 }
